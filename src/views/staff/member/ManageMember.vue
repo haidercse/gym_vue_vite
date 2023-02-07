b850b4c955a73b97221db3a0ff1aff281f61471b
haider-cse <haider.cse7644@gmail.com>
2023-02-07 11:01:56 -0600

    delete not yet

--- src/views/staff/member/ManageMember.vue
+++ src/views/staff/member/ManageMember.vue
@@ -1,5 +1,4 @@
 <template>
-
   <main id="main-container">
     <!-- Page Content -->
     <div class="content">
@@ -26,13 +25,30 @@
               <thead>
                 <tr>
                   <th>Name</th>
+                  <th>Id</th>
+                  <th>Mobile Number</th>
+                  <th>Card No </th>
+                  <th>Start Date </th>
+                  <th>Status </th>
+                  <th>Image</th>
+                  <th>Create By </th>
                   <th class="text-center" style="width: 100px">Actions</th>
                 </tr>
               </thead>
               <tbody>
-                <tr v-for="(member,id) in all_members" :key="id">
+                <tr v-for="(member, id) in all_members" :key="id">
                   <td class="font-w600 font-size-sm">{{ member.name }}</td>
-
+                  <td>{{ member.member_id }}</td>
+                  <td>{{ member.mobile_number }}</td>
+                  <td>{{ member.card_no }}</td>
+                  <td>{{ member.start_date ?? 'N/A' }}</td>
+                  <td>
+                    <span class="badge" :class=" getStatusColor(member.status)">{{ getStatusName(member.status) }}</span>
+                  </td>
+                  <td>
+                    <img :src="member.image" :alt=" member.name " width="82px">
+                  </td>
+                  <td>{{ member.user.name }}</td>
                   <td class="text-center">
                     <div class="btn-group">
                       <button
@@ -59,6 +75,47 @@
               </tbody>
             </table>
           </div>
+          <nav aria-label="Page navigation">
+            <ul class="pagination justify-content-end">
+              <li class="page-item">
+                <a
+                  class="page-link"
+                  href="javascript:void(0)"
+                  tabindex="-1"
+                  aria-label="Previous"
+                >
+                  <span aria-hidden="true">
+                    <i class="fa fa-angle-double-left"></i>
+                  </span>
+                  <span class="sr-only">Previous</span>
+                </a>
+              </li>
+              <li class="page-item active">
+                <a class="page-link" href="javascript:void(0)">1</a>
+              </li>
+              <li class="page-item">
+                <a class="page-link" href="javascript:void(0)">2</a>
+              </li>
+              <li class="page-item disabled">
+                <a class="page-link" href="javascript:void(0)">3</a>
+              </li>
+              <li class="page-item">
+                <a class="page-link" href="javascript:void(0)">4</a>
+              </li>
+              <li class="page-item">
+                <a
+                  class="page-link"
+                  href="javascript:void(0)"
+                  aria-label="Next"
+                >
+                  <span aria-hidden="true">
+                    <i class="fa fa-angle-double-right"></i>
+                  </span>
+                  <span class="sr-only">Next</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
         </div>
       </div>
       <!-- END Full Table -->
@@ -68,7 +125,7 @@
 </template>
 
 <script>
-import { mapActions,mapState } from "vuex";
+import { mapActions, mapState } from "vuex";
 import MainDashboardHeader from "../../../components/template/MainDashboardHeader.vue";
 export default {
   components: {
@@ -79,11 +136,25 @@ export default {
   },
   methods: {
     ...mapActions(["getAllMember"]),
+    getStatusName(value){
+      if(value == 0){
+        return 'Inactive';
+      }else if(value == 1){
+        return 'Active';
+      }
+    },
+    getStatusColor(color){
+      if(color == 0){
+        return 'badge-danger';
+      }else if(color == 1){
+        return 'badge-success';
+      }
+    }
   },
   computed: {
     ...mapState({
       all_members: (state) => state.member.members,
-    })
+    }),
   },
 };
 </script>
